697af72f8523295867dcacfc60831dcc10f6f403
mihai <mihail.adc@gmail.com>
2018-03-24 14:03:25 +0000

    relayWorking

--- NurseCall/relayCommand.py
+++ NurseCall/relayCommand.py
@@ -2,18 +2,18 @@ import serial
 import debug as D
 
 
-serPort = serial.Serial('/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2:1.0', 19200, timeout=1)
+#serPort = serial.Serial('/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0', 19200, timeout=1)
 
-#serPort = serial.Serial('/dev/ttyACM0', 19200, timeout=1)
+serPort = serial.Serial('/dev/ttyACM0', 19200, timeout=1)
 
 def setRelay(id):
 	D.P("Setting relay " + str(id) + " ON")
-	msg = "relay ON " + str(id) + "\n\r"
+	msg = "relay on " + str(id) + "\n\r"
 	serPort.write(msg)
 	D.P("Serial sent: " + msg)
 def unsetRelay(id):
 	D.P("Setting relay " + str(id) + " OFF")
-	msg = "relay OFF " + str(id) + "\n\r"
+	msg = "relay off " + str(id) + "\n\r"
 	serPort.write(msg)
 	D.P("Serial sent: " + msg)
 
